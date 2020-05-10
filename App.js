0aa9586f5d14e3316b707e1baef4462e7df9c20d
Sergey Komarov <anaxronik@gmail.com>
2020-05-10 19:14:24 +0500

    add fonts and preloader functions

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { AppLoading } from "expo";
+import { bootstrap } from "./src/bootstrap";
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  // preloader function
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={bootstrap}
+        onFinish={() => setIsReady(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
