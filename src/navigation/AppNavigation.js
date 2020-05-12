29e55a88b188f15e166029b3fc52acaf53386adf
Sergey Komarov <anaxronik@gmail.com>
2020-05-12 18:47:36 +0500

    create bottom navigator

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -10,44 +10,90 @@ import { THEME } from "../theme";
 import PhotoIcon from "../components/PhotoIcon";
 import BookedIcon from "../components/BookedIcon";
 import BurgerIcon from "../components/BurgerIcon";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { View, Text } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const BottomTabs = createBottomTabNavigator();
 const PostNavigator = createStackNavigator();
+const PostNavigatorContainer = () => (
+  <PostNavigator.Navigator
+    // default settings for header
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: THEME.MAIN_COLOR,
+      },
+      headerTintColor: "#fff",
+    }}
+  >
+    <PostNavigator.Screen
+      name="Main"
+      component={MainScreen}
+      options={{
+        title: "Главный экран",
+        headerRight: () => <PhotoIcon />,
+        headerLeft: () => <BurgerIcon />,
+      }}
+    />
+    <PostNavigator.Screen
+      name="Post"
+      component={PostScreen}
+      options={({ route }) => ({
+        title: route.params.postTitle,
+        headerRight: () => <BookedIcon booked={route.params.postIsBooked} />,
+      })}
+    />
+    <PostNavigator.Screen name="About" component={AboutScreen} />
+    <PostNavigator.Screen name="Create" component={CreateScreen} />
+    <PostNavigator.Screen name="Booked" component={BookedScreen} />
+  </PostNavigator.Navigator>
+);
 
 export const AppNavigation = () => {
   return (
     <NavigationContainer>
-      <PostNavigator.Navigator
-        // default settings for header
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: THEME.MAIN_COLOR,
-          },
-          headerTintColor: "#fff",
+      <BottomTabs.Navigator
+        tabBarOptions={{
+          activeTintColor: THEME.MAIN_COLOR,
         }}
       >
-        <PostNavigator.Screen
-          name="Main"
-          component={MainScreen}
+        <BottomTabs.Screen
+          name="POST"
+          component={PostNavigatorContainer}
           options={{
-            title: "Главный экран",
-            headerRight: () => <PhotoIcon />,
-            headerLeft: () => <BurgerIcon />,
+            tabBarLabel: "ПОСТЫ",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="chat" size={size} color={color} />
+            ),
           }}
         />
-        <PostNavigator.Screen
-          name="Post"
-          component={PostScreen}
-          options={({ route }) => ({
-            title: route.params.postTitle,
-            headerRight: () => (
-              <BookedIcon booked={route.params.postIsBooked} />
+        <BottomTabs.Screen
+          name="BOOKED"
+          component={SettingsScreen}
+          options={{
+            tabBarLabel: "ЗАКЛАДКИ",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="star" size={size} color={color} />
             ),
-          })}
+          }}
         />
-        <PostNavigator.Screen name="About" component={AboutScreen} />
-        <PostNavigator.Screen name="Create" component={CreateScreen} />
-        <PostNavigator.Screen name="Booked" component={BookedScreen} />
-      </PostNavigator.Navigator>
+      </BottomTabs.Navigator>
     </NavigationContainer>
   );
 };
